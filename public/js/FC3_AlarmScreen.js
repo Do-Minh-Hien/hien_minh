e234e077e792d36f0593bc118aee7f519c0563c3
Do-Minh-Hien <104559493+Do-Minh-Hien@users.noreply.github.com>
2022-05-19 23:50:03 +0700

    first commit

---
+++ public/js/FC3_AlarmScreen.js
@@ -0,0 +1,29 @@
+// Chương trình con đọc dữ liệu SQL
+function fn_Alarm_Show(){
+    socket.emit("msg_Alarm_Show", "true");
+    socket.on('Alarm_Show',function(data){
+        fn_table_Alarm(data);
+    }); 
+}
+ 
+// Chương trình con hiển thị SQL ra bảng
+function fn_table_Alarm(data){
+    if(data){
+        $("#table_Alarm tbody").empty(); 
+        var len = data.length;
+        var txt = "<tbody>";
+        if(len > 0){
+            for(var i=0;i<len;i++){
+                    txt += "<tr><td>"+data[i].date_time
+                        +"</td><td>"+data[i].ID
+                        +"</td><td>"+data[i].Status
+                        +"</td><td>"+data[i].AlarmName
+                        +"</td></tr>";
+                    }
+            if(txt != ""){
+            txt +="</tbody>"; 
+            $("#table_Alarm").append(txt);
+            }
+        }
+    }   
+}
